425fc55c4ac3138e639925a07a85245cc9f6f118
Robert Lavergne <rlavergne@pandora.com>
2017-07-16 21:39:02 -0700

    removed periods

--- src/reducers/ProjectsReducer.js
+++ src/reducers/ProjectsReducer.js
@@ -7,9 +7,9 @@ export default function() {
       company: 'Pandora Radio',
       summary: 'By this time I had built quite a few stations at Pandora using our template. Including other ones that required a bit of customizing like "Pandora Sessions". This time, however, I begged my boss and traded assignments with a teammate for this one because I wanted to work on it BAD. It was a lot of customization to this one because having a radio show by Questlove was a HUGE deal to the company. I refused to let them down. A year later and quite a few updates later, we customized it further into Questlove 2.0, bringing back the archive of older shows and a new format with segmented episodes.',
       imgLinks: [
-        './img/questlove/questlove1.png',
-        './img/questlove/questlove2.png',
-        './img/questlove/questlove3.png'
+        '/img/questlove/questlove1.png',
+        '/img/questlove/questlove2.png',
+        '/img/questlove/questlove3.png'
       ],
       imgAlt: 'Questlove Supreme Screenshots',
       skills: ['HTML / CSS', 'Apache Velocity', 'Perforce'],
@@ -23,9 +23,9 @@ export default function() {
       company: 'Pandora Radio',
       summary: 'Pandora has a special way of finding the next best song to play, and that is what they like to call the Music Genome. The marketing team decided that SXSW 2017 was the perfect place to get the people more educated about exactly what makes Pandora so special. So I was assigned with the task of building the interactive Genome Dome. Basically a 1-page iPad "app" that people could play with at the SXSW setup in Austin. Originally, I used CSS Keyframes and animations to make a pulse. BUT, since that was not very iPad friendly, I managed to make a few JS/JQuery functions to get the job done. Not to mention I had instruct the team in Austin on how to put the iPads in "Kiosk Mode" so people couldnt use the iPad for other things.',
       imgLinks: [
-        './img/genomedome/genomedome1.png',
-        './img/genomedome/genomedome2.png',
-        './img/genomedome/genomedome3.png'
+        '/img/genomedome/genomedome1.png',
+        '/img/genomedome/genomedome2.png',
+        '/img/genomedome/genomedome3.png'
       ],
       imgAlt: 'Genome Dome Screenshots',
       skills: ['HTML5 / CSS3', 'Javascript/JQuery', 'Perforce']
@@ -37,9 +37,9 @@ export default function() {
       company: 'Moxie USA',
       summary: 'After the company merged with a smaller agency, it was time for some rebranding and a new website (that was long overdue in the first place). A new team was assembled that included myself, my manager, and 3 other developers from our other offices. My manager being the Senior Dev on the team meant it was a lot of back and forth, hands on discussion about the direction we should take. I was the right hand on this project and my involvement was quite large. Once again I built the responsive navigation, but also developed the "locations" section with dynamic google maps images pulled in from the API. It was also my job to help compile the code from devs working on other sections of the site to make sure it meshed well with what we had setup in our core code base. As usual it was a responsive site that had many features we had to make sure were friendly with all browsers and devices, including IE8. At the time this was my Crown Jewel when it came to sites I worked on.',
       imgLinks: [
-        './img/moxie/moxie1.png',
-        './img/moxie/moxie2.png',
-        './img/moxie/moxie3.png'
+        '/img/moxie/moxie1.png',
+        '/img/moxie/moxie2.png',
+        '/img/moxie/moxie3.png'
       ],
       imgAlt: 'MoxieUSA.com Screenshots',
       skills: ['Javascript/JQuery', 'Google Maps API', 'CSS3 Animations', 'Custom Responsive Grid', '.NET/ASP (SiteCore)', 'Cross-Browser back to IE8'],
@@ -53,9 +53,9 @@ export default function() {
       company: 'Moxie USA',
       summary: 'I start out as an intern, and this was my first project. Help the senior designer "retrofit" our clients website. Basically I played a large role in taking this website and making it responsive. That turned into a Webby Nomination for the company. We lost to Web-MD I think. Far from your average internship I would say. For the most part it was taking a lot of pre-written code and making sure each assigned module or section was made responsive for all browsers/screens at the time. This took a lot of learning my way around the .NET/ASP content management framework it was built in, and making sure not to step on any toes. Despite it being my first big assignment for the company, the Senior Dev was impressed with how effecient I completed each task. My biggest contribution to the project was the hamburger menu that appears on mobile devices.',
       imgLinks: [
-        './img/strong4life/strong4life1.png',
-        './img/strong4life/strong4life2.png',
-        './img/strong4life/strong4life3.png'
+        '/img/strong4life/strong4life1.png',
+        '/img/strong4life/strong4life2.png',
+        '/img/strong4life/strong4life3.png'
       ],
       imgAlt: 'Strong4Life.com Screenshots',
       skills: ['Javascript/Jquery', 'LESS', 'Extra Stength: Responsive Framework', '.NET/ASP (SiteCore)', 'Cross-Browser back to IE8'],
@@ -69,9 +69,9 @@ export default function() {
       company: 'Deep Blue Design',
       summary: 'Probably my biggest project at the time, I was in charge of building the entire user facing side of this website. I introduced some parallaxing skills when I started so the designer wanted to see if we could make that happen for this website. Not exactly my best work but it was impressive to the client nonetheless. Sadly it hasnt been updated in a looonnng time and the parallaxing did not transfer well once it went live. I am honestly surprised it is still up and running today. Like all websites that came through Deep Blue, it had to be responsive as well. This one was a major learning experience.',
       imgLinks: [
-        './img/poseidon/poseidon1.png',
-        './img/poseidon/poseidon2.png',
-        './img/poseidon/poseidon3.png'
+        '/img/poseidon/poseidon1.png',
+        '/img/poseidon/poseidon2.png',
+        '/img/poseidon/poseidon3.png'
       ],
       imgAlt: 'Poseidon Screenshots',
       skills: ['HTML5 / CSS3', 'Javascript/JQuery', 'Parallax Imagry', 'Pulse CMS', 'ZURB Foundation 3' ],
